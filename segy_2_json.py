374e43c6671617dda6b416a653701172a2f11bf6
Martin Mochetti <martin.mochetti@gmail.com>
2022-11-14 09:27:23 -0300

    Initial version of segy_2_json.

---
+++ segy_2_json.py
@@ -0,0 +1,93 @@
+import re
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import segyio
+import json
+import sys
+
+filename = sys.argv[1]
+
+def parse_trace_headers(segyfile, n_traces):
+    '''
+    Parse the segy file trace headers into a pandas dataframe.
+    Column names are defined from segyio internal tracefield
+    One row per trace
+    '''
+    # Get all header keys
+    headers = segyio.tracefield.keys
+    # Initialize dataframe with trace id as index and headers as columns
+    df = pd.DataFrame(index=range(1, n_traces + 1),
+                      columns=headers.keys())
+    # Fill dataframe with all header values
+    for k, v in headers.items():
+        df[k] = segyfile.attributes(v)[:]
+    return df
+
+
+def parse_text_header(segyfile):
+    '''
+    Format segy text header into a readable, clean dict
+    '''
+    raw_header = segyio.tools.wrap(segyfile.text[0])
+    # Cut on C*int pattern
+    cut_header = re.split(r'C ', raw_header)[1::]
+    # Remove end of line return
+    text_header = [x.replace('\n', ' ') for x in cut_header]
+    text_header[-1] = text_header[-1][:-2]
+    # Format in dict
+    clean_header = {}
+    i = 1
+    for item in text_header:
+        key = "C" + str(i).rjust(2, '0')
+        i += 1
+        clean_header[key] = item
+    return clean_header
+
+with segyio.open(filename, ignore_geometry=True) as f:
+    # Get basic attributes
+    n_traces = f.tracecount
+    sample_rate = segyio.tools.dt(f) / 1000
+    n_samples = f.samples.size
+    twt = f.samples
+    data = f.trace.raw[:]  # Get all data into memory (could cause on big files)
+    # Load headers
+    bin_headers = f.bin
+    text_headers = parse_text_header(f)
+    trace_headers = parse_trace_headers(f, n_traces)
+
+
+clip_percentile = 99
+vm = np.percentile(data, clip_percentile)
+
+json_obj = {
+  'percentile_99': vm,
+}
+
+json_obj['text_headers'] = text_headers
+
+json_obj['binary_headers'] = {}
+
+for key in bin_headers:
+    json_obj['binary_headers'][key.__str__()] = bin_headers.get(key)
+
+json_obj['traces'] = []
+
+for idx, trace in enumerate(data):
+    trace_obj = { 'headers': dict(trace_headers.loc[idx + 1]), 'samples': trace }
+    json_obj['traces'].append(trace_obj)
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+json_result = json.dumps(json_obj, indent = 4, cls = NpEncoder)
+
+with open('test.json', 'w') as outfile:
+    outfile.write(json_result)
